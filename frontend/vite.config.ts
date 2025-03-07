a91ca434096b82e2b5c909ed90d3c6f496137fe1
Ram <153852265+RamChandra1528@users.noreply.github.com>
2025-03-07 23:11:58 +0530

    update config file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,35 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    strictPort: true,
+    port: 3000,
+    mimeTypes: {
+      'application/javascript': ['js', 'ts', 'tsx']
+    }
+  },
+  build: {
+    target: 'esnext',
+    outDir: 'dist',
+  },
+  esbuild: {
+    loader: 'tsx',
+  },
 });
+  
